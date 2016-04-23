319d34f626799dd1e254b78da1594c3e0dba4b56
gmiesch <gvonrose@trinity.edu>
2016-04-23 11:17:20 -0500

    Added Player prefab and first person movement with test scene.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour
+{
+
+    float lookSensitivity = 3;
+    float yRotation;
+    float xRotation;
+    public float currentXRotation;
+    public float currentYRotation;
+    float yRotationV;
+    float xRotationV;
+    float lookSmoothness = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        yRotation = currentYRotation;
+        xRotation = currentXRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -80, 100); //Locking how far you can look down and up
+
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothness);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothness);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 6.0F;
+    public float jumpSpeed;
+    public float gravity = 20.0F;
+    public float rotateSpeed = 6;
+    private Vector3 moveDirection = Vector3.zero;
+    public GameObject cameraObject;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+
+        transform.rotation = Quaternion.Euler(0, cameraObject.GetComponent<MouseLook>().currentYRotation, 0);
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
