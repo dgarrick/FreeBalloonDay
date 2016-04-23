9e80652ac70ca54157acc11632004903a4734520
dgarrick <danieljamesgarrick@gmail.com>
2016-04-23 16:33:36 -0500

    button that draws lines for spikes

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ButtonScript : MonoBehaviour {
+    float pressTime = 0f;
+    Boolean enables = true;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && enabled)
+        {
+            GameObject[] spikes = GameObject.FindGameObjectsWithTag("Spike");
+            Debug.Log("Player hit the button!");
+            foreach (GameObject go in spikes)
+            {
+                Debug.Log("wow!");
+                Color c1 = Color.red;
+                Color c2 = Color.red;
+                int lengthOfLineRenderer = 120;
+                LineRenderer lineRenderer = go.AddComponent<LineRenderer>();
+                lineRenderer.SetColors(c1, c2);
+                lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+                lineRenderer.SetWidth(1F, 1F);
+                lineRenderer.SetVertexCount(lengthOfLineRenderer);
+                pressTime = Time.time;
+                int i = 0;
+                float x = go.transform.position.x;
+                float y = 60 + go.transform.position.y;
+                float z = go.transform.position.z;
+                while (i < lengthOfLineRenderer)
+                {
+                    //Debug.Log("x " + x + " y " + (y-i) + " z " + z);
+                    Vector3 pos = new Vector3(x, y - i, z);
+                    lineRenderer.SetPosition(i, pos);
+                    i++;
+                }
+            }
+            enabled = false;
+            StartCoroutine(waitAndKillLasers(5));
+        }
+    }
+
+    IEnumerator waitAndKillLasers(int waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        GameObject[] spikes = GameObject.FindGameObjectsWithTag("Spike");
+            Debug.Log("Time's up!");
+            foreach (GameObject go in spikes)
+            {
+                LineRenderer lineRenderer = go.GetComponent<LineRenderer>();
+                lineRenderer.enabled = false;
+            }
+    }
+}
