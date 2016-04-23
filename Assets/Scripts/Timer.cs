4153eebddb6179cd0f1415ad442e4896a7aa1a45
gmiesch <gvonrose@trinity.edu>
2016-04-23 12:02:31 -0500

    Added a working timer prefab.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float gameTimer;
+    private GameObject timerText;
+
+	// Use this for initialization
+	void Start () {
+        gameTimer = 60.0f;
+        timerText = GameObject.Find("TimerText");
+	}
+	
+    void FixedUpdate () {
+        gameTimer -= Time.deltaTime;
+        timerText.GetComponent<Text>().text = Mathf.Floor(gameTimer).ToString();
+        Debug.Log(gameTimer.ToString());
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
