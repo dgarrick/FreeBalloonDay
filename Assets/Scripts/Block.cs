074000f57769a93a06f1a310a63dc54f0027fbd7
campbellcompton <campbellcompton@yahoo.com>
2016-04-28 14:56:51 -0500

    Adding ice and immovable blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,6 +7,8 @@ public class Block : MonoBehaviour {
 	BoxCollider[] colliders;
 	Vector3 myPosition;
 	GameObject middle;
+	public bool isIce;
+	public bool isImmovable;
 	// Use this for initialization
 	void Start () {
 		blocks = GameObject.FindObjectsOfType<Block> ();
@@ -21,28 +23,40 @@ public class Block : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+		if (other.gameObject.tag == "Player" && !isImmovable)
         {
             Vector3 myPos = gameObject.transform.position;
             Vector3 playerPos = other.gameObject.transform.position;
             Vector3 diff = playerPos - myPos;
             bool xLarger = Mathf.Abs(diff.x) > Mathf.Abs(diff.z);
 			if (xLarger && diff.x > 0)
-				tryMove (new Vector3(-4, 0, 0) + myPosition);
+				tryMove (new Vector3(-4, 0, 0));
 			else if (xLarger && diff.x < 0)
-				tryMove(new Vector3 (4, 0, 0) + myPosition);
+				tryMove(new Vector3 (4, 0, 0));
 			else if (!xLarger && diff.z > 0)
-				tryMove(new Vector3(0, 0, -4) + myPosition);
+				tryMove(new Vector3(0, 0, -4));
             else if (!xLarger && diff.z < 0)
-				tryMove(new Vector3(0, 0, 4) + myPosition);
+				tryMove(new Vector3(0, 0, 4));
             else
                 Debug.Log("Something very bad happened");
         }
     }
 
-	private void tryMove(Vector3 position) {
-		if (!hasBlock (position) && isInBounds(position))
-			StartCoroutine (changePosition (position));
+	private void tryMove(Vector3 direction) {
+		// Normal blocks. Just move one unit in direction
+		if (!isIce) {
+			Vector3 newPosition = direction + myPosition;
+			if (!hasBlock (newPosition) && isInBounds (newPosition))
+				StartCoroutine (changePosition (newPosition));
+		}
+		// Ice blocks. Move until they can no longer move.
+		else if (isIce) {
+			Vector3 newPosition = myPosition;
+			while (!hasBlock (newPosition + direction) && isInBounds(newPosition + direction)) {
+				newPosition += direction;
+			}
+			StartCoroutine (changePosition(newPosition));
+		}
 	}
 
 	public bool isInBounds(Vector3 position) {
@@ -62,12 +76,13 @@ public class Block : MonoBehaviour {
 		return ret;
 	}
 
-    private IEnumerator changePosition(Vector3 newPos) {
+	private IEnumerator changePosition(Vector3 newPos) {
         foreach (BoxCollider coll in colliders)
         {
             coll.enabled = false;
         }
-        float time = 0.5f;  
+		// Blocks are 4 units. 4 units = 0.5 seconds.
+		float time = Vector3.Distance (newPos, myPosition) / 8.0f; 
         float i = 0.0f;
         float rate = 1.0f/time;
         Vector3 oldPos = transform.position;
