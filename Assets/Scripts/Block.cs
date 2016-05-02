1d6607b3748f19e43dfd61a394e045ac564abb25
Daniel Garrick <dgarrick@trinity.edu>
2016-05-01 19:43:55 -0500

    blocks shoot out dat der beam of light

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,14 +9,58 @@ public class Block : MonoBehaviour {
 	GameObject middle;
 	public bool isIce;
 	public bool isImmovable;
+	private bool enabled;
+	private LineRenderer lineRenderer;
+	private Material laserMat;
+	private Color c1;
+	private Color c2;
+	private int lengthOfLineRenderer;
+	//private ParticleEmitter emitter;
+	//private Particle[] particles;
+
 	// Use this for initialization
 	void Start () {
 		blocks = GameObject.FindObjectsOfType<Block> ();
 		middle = GameObject.Find ("Middle");
 		myPosition = gameObject.transform.position;
 		colliders = GetComponents<BoxCollider>();
+		enabled = false;
+
+		//line renderer properties
+		laserMat = Resources.Load("Materials/Cone") as Material;
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.Add
+		c1 = Color.white;
+		c2 = Color.white;
+		lengthOfLineRenderer = 120;
+		lineRenderer.transform.parent = gameObject.transform;
+		lineRenderer.enabled = true;
+		lineRenderer.SetColors(c1, c2);
+		lineRenderer.material = laserMat;
+		lineRenderer.SetWidth(1F, 1F);
+		lineRenderer.SetVertexCount(lengthOfLineRenderer);
+
+		//line particles
+		//emitter = gameObject.AddComponent<ParticleEmitter>();
+		//particles = emitter.particles;
+		//Debug.Log (particles.Length);
+		drawLine ();
 	}
-	
+
+	void drawLine() {
+		if (!lineRenderer.enabled)
+			lineRenderer.enabled = true;
+		int i = 0;
+		float x = gameObject.transform.position.x;
+		float y = 60 + gameObject.transform.position.y;
+		float z = gameObject.transform.position.z;
+		while (i < lengthOfLineRenderer) {
+			Vector3 pos = new Vector3 (x, y - i, z);
+			lineRenderer.SetPosition(i, pos);
+			i++;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 	}
@@ -81,13 +125,15 @@ public class Block : MonoBehaviour {
         {
             coll.enabled = false;
         }
+		lineRenderer.enabled = false;
 		// Blocks are 4 units. 4 units = 0.5 seconds.
-		float time = Vector3.Distance (newPos, myPosition) / 8.0f; 
+		float time = Vector3.Distance (newPos, myPosition) / 12.0f; 
         float i = 0.0f;
         float rate = 1.0f/time;
         Vector3 oldPos = transform.position;
         while (i < 1.0f) {
             i += Time.deltaTime * rate;
+			drawLine ();
             gameObject.transform.position = Vector3.Lerp(oldPos, newPos, i);
             yield return null;
         }
